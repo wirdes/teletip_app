58070903e9aff7ed066a115098ecd86e8807c5c1
wirdes <45904810+wirdes@users.noreply.github.com>
2022-01-13 17:58:44 +0300

    fix: :zap: fiz app_colors file

--- lib/app_colors.dart
+++ lib/app_colors.dart
@@ -7,11 +7,4 @@ var appPrimaryColor = HexColor("#142A3B");
 var appPrimaryColorLight = HexColor("#5574F7");
 const appPrimaryColorHex = Color.fromRGBO(40, 59, 114, 1);
 
-const kPrimaryColor = Color(0xFF00BF6D);
-const kSecondaryColor = Color(0xFFFE9901);
-const kContentColorLightTheme = Color(0xFF1D1D35);
-const kContentColorDarkTheme = Color(0xFFF5FCF9);
-const kWarninngColor = Color(0xFFF3BB1C);
-const kErrorColor = Color(0xFFF03738);
-
 const kDefaultPadding = 20.0;
